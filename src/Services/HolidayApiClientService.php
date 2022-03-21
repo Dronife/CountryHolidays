f413c46723fa427b125c0cb4ab617ef8391dc6d8
Dronife <dimassss9999@gmail.com>
2022-03-21 14:40:39 +0200

    Update service

--- src/Services/HolidayApiClientService.php
+++ src/Services/HolidayApiClientService.php
@@ -19,57 +19,12 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class HolidayApiClientService
 {
-
-    private string $kayaposoftBaseApiUrl;
-    private ApiRequest $apiRequest;
-
-    public function __construct(
-        string $kayaposoftBaseApiUrl,
-        ApiRequest $apiRequest
-    ) {
-        $this->kayaposoftBaseApiUrl = $kayaposoftBaseApiUrl;
-        $this->apiRequest = $apiRequest;
+    public function __construct()
+    {
     }
-
-
-//    public function getOneHolidayModel(Country $country, string $date): HolidayModel
-//    {
-//        return $this->apiRequest
-//            ->get($this->getUrlForSpecificHolidayDate($country, $date), 'array<' . HolidayModel::class . '>')[0];
-//    }
-
 
     public function isFreeDay(string $date): bool
     {
         return Carbon::parse($date)->isWeekend();
     }
-
-    private function getUrlForDayCheck(Country $country, string $date): string
-    {
-        return sprintf(
-            '%sisPublicHoliday&date=%s&country=%s',
-            $this->kayaposoftBaseApiUrl,
-            $date,
-            $country->getCountryCode()
-        );
-    }
-
-//    public function getUrlForSpecificHolidayDate(Country $country, string $date): string
-//    {
-//        return sprintf(
-//            '%sgetHolidaysForDateRange&fromDate=%s&toDate=%s&country=%s',
-//            $this->kayaposoftBaseApiUrl,
-//            $date,
-//            $date,
-//            $country->getCountryCode()
-//        );
-//    }
-
-    public function isPublicHoliday(Country $country, string $date): bool
-    {
-        return $this->apiRequest
-            ->get($this->getUrlForDayCheck($country, $date), IsPublicHolidayModel::class)
-            ->isPublicHoliday();
-    }
-
 }
